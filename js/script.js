c861ab14ac4874118dc6225351a4288b8bbd3d12
Maicon Douglas <mdprogamacao@outlook.com>
2025-12-26 22:37:58 -0300

    criando funcionalidade de cancelar ação, de excluir tarefa, de adicionar a tarefa, e de editar tarefa.

--- js/script.js
+++ js/script.js
@@ -1,48 +1,115 @@
-const todoform = document.querySelector("#todo-form");
+
+const todoForm = document.querySelector("#todo-form"); 
 const todoInput = document.querySelector("#todo-input");
-const todoList = document.querySelector("#todo-list");
-const todoForm = document.querySelector("#edit-form");
-const editform = document.querySelector("#edit-input");
+const todoList = document.querySelector("#todo-list"); 
+const editForm = document.querySelector("#edit-form"); 
+const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
 
+let oldInputValue;
 const saveTodo = (text) => {
+   
     const todo = document.createElement('div');
     todo.classList.add("todo");
 
-    const todotitle = document.createElement('h3');
-    todotitle.innerText = text;
-    todo.appendChild(todotitle)
 
+    const todoTitle = document.createElement('h3');
+    todoTitle.innerText = text;
+    todo.appendChild(todoTitle);
 
-    const editBtn = document.createElement('button');
-editBtn.classList.add('edit-todo')
-editBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
-    todo.appendChild(editBtn);
-
-        const  doneBtn = document.createElement('button');
-    doneBtn.classList.add('finish-todo')
-    doneBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+ 
+    const doneBtn = document.createElement('button');
+    doneBtn.classList.add('finish-todo');
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
     todo.appendChild(doneBtn);
 
 
-       const  deleteBtn = document.createElement('button');
-    deleteBtn.classList.add('delete-todo')
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('edit-todo');
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+    todo.appendChild(editBtn);
+
+  
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('remove-todo'); 
     deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
-    todo.append(deleteBtn)
+    todo.appendChild(deleteBtn);
 
-    todoList.appendChild(todo)
+    
+    todoList.appendChild(todo);
 
+  
     todoInput.value = "";
-    todoInput.focus()
+    todoInput.focus();
 }
 
-todoform.addEventListener("submit", (event) => {
-    event.preventDefault()
+
+const toggleForms = () => {
+    editForm.classList.toggle('hide');
+    todoForm.classList.toggle('hide');
+
+};
+
+const updateTodo = (text) => {
+    const todos = document.querySelectorAll('.todo');
+    todos.forEach((todo) => {
+        let todoTitle = todo.querySelector('h3');
+
+        if(todoTitle.innerText === oldInputValue){
+            todoTitle.innerText = text;
+        }
+    })
+}
+
+todoForm.addEventListener("submit", (event) => {
+    event.preventDefault(); 
 
     const inputValue = todoInput.value;
     
-    if(inputValue){
-        saveTodo(inputValue)
+    if (inputValue) {
+        saveTodo(inputValue);
     }
+});
+
+
+document.addEventListener('click', (event)=> {
+    const targetEl = event.target;
+    const parentEl = targetEl.closest('div');
+    let todoTitle;
+
+    if(parentEl && parentEl.querySelector('h3')){
+        todoTitle = parentEl.querySelector('h3').innerText;
+    }
+
+    if(targetEl.classList.contains('finish-todo')){
+        parentEl.classList.add('done')
+    }
+    if(targetEl.classList.contains('remove-todo')){
+        parentEl.remove();
+    }
+    if(targetEl.classList.contains('edit-todo')){
+        toggleForms();
+
+        editInput.value = todoTitle;
+        oldInputValue = todoTitle;
+    }
+})
+
+cancelEditBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    toggleForms();
+})
+
+
+editForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const editInputValue = editInput.value;
+
+    if(editInput){
+        updateTodo(editInputValue);
+    }
+    toggleForms();
 })
